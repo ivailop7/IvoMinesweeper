ad0db8a51a3e3097ae8cc42fb4b028715853a1bd
Ivaylo Pavlov <ivailo90@gmail.com>
2020-02-22 17:06:27 +0000

    add react95 library, folder reorg

---
+++ src/App/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { reset, themes, List, ListItem, Divider } from "react95";
+
+const ResetStyles = createGlobalStyle`
+  ${reset}
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <ResetStyles />
+      <ThemeProvider theme={themes.default}>
+        <List>
+          <ListItem>🎤 Sing</ListItem>
+          <ListItem>💃🏻 Dance</ListItem>
+          <Divider />
+          <ListItem disabled>😴 Sleep</ListItem>
+        </List>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
